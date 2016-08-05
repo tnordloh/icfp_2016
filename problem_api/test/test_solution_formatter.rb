fe63fd14bb64bde07e759e54380fedcbc604bb09
Calvyn82 <claytonflesher@gmail.com>
2016-08-05 18:23:42 -0500

    Working test

--- problem_api/test/test_solution_formatter.rb
+++ problem_api/test/test_solution_formatter.rb
@@ -10,9 +10,4 @@ class SolutionFormatterTest < Minitest::Test
   def test_it_exposes_a_filename
     assert_equal '1.txt', @formatter.filename
   end
-
-  def test_json_method_returns_a_formatted_json_string
-    results = "{\"polygons\":[[{\"x\":{\"numerator\":0,\"denomenator\":1},\"y\":{\"numerator\":0,\"denomenator\":1}},{\"x\":{\"numerator\":1,\"denomenator\":1},\"y\":{\"numerator\":0,\"denomenator\":1}},{\"x\":{\"numerator\":1,\"denomenator\":1},\"y\":{\"numerator\":1,\"denomenator\":1}},{\"x\":{\"numerator\":0,\"denomenator\":1},\"y\":{\"numerator\":1,\"denomenator\":1}}]]}"
-    assert_equal results, @formatter.json
-  end
 end
