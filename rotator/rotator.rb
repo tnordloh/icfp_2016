310850e2ab0147f754cc8611f472708a9399e48c
Calvyn82 <claytonflesher@gmail.com>
2016-08-06 11:48:57 -0500

    Current rotator

--- rotator/rotator.rb
+++ rotator/rotator.rb
@@ -1,31 +1,31 @@
 class Rotator
   def self.rotate(cx, cy, angle, point)
     rad_angle = angle * Math::PI / 180
-    s = Math.sin(rad_angle)
-    c = Math.cos(rad_angle)
+    s = Math.sin(rad_angle).to_r
+    c = Math.cos(rad_angle).to_r
 
     #translate point back to origin
-    point.x -= cx.to_f
-    point.y -= cy.to_f
+    point.x -= cx.to_r
+    point.y -= cy.to_r
 
     # rotate point
     xnew = (point.x * c - point.y * s)
     ynew = (point.x * s + point.y * c)
 
     # translate back
-    point.x = (xnew + cx.to_f).to_r
-    point.y = (ynew + cy.to_f).to_r
+    point.x = (xnew + cx.to_r).to_r
+    point.y = (ynew + cy.to_r).to_r
 
     puts "#{point.x},#{point.y}"
   end
 end
 
 Point = Struct.new(:x, :y)
-cx = ARGV.shift.to_f
-cy = ARGV.shift.to_f
-angle = ARGV.shift.to_f
+cx = ARGV.shift.to_r
+cy = ARGV.shift.to_r
+angle = ARGV.shift.to_r
 p = Point.new
-p.x = ARGV.shift.to_f
-p.y = ARGV.shift.to_f
+p.x = ARGV.shift.to_r
+p.y = ARGV.shift.to_r
 
 Rotator.rotate(cx, cy, angle, p)
