08c923e2405f1c74dee11bb5db4af1a410721ddd
Timothy Nordloh <tnordloh@gmail.com>
2016-08-06 12:19:07 -0500

    tweak solver

--- solver/lib/solver.rb
+++ solver/lib/solver.rb
@@ -3,27 +3,29 @@ require_relative "problem"
 require_relative "../../lib/problem_api"
 
 class Solver
-  def self.solve
-    new.solve
+  def self.solve(zeroes_only = false)
+    new(zeroes_only).solve
   end
 
-  def initialize
+  def initialize(zeroes_only = false)
     @files =
       Dir
         .glob(File.join(__dir__, *%w[.. .. problems *.txt]))
         .select { |f| f =~ /\b\d+\.txt\z/ }
     @api = ProblemAPI.new
     @db = Database.new
+    @zeroes_only = zeroes_only
   end
 
-  attr_reader :files, :api, :db
-  private     :files, :api, :db
+  attr_reader :files, :api, :db, :zeroes_only
+  private     :files, :api, :db, :zeroes_only
 
   def solve
     files.each do |path|
       number = File.basename(path, ".txt").to_i
 
       next if db.best_score(number) == 1.0
+      next if db.best_score(number) != 0.0 and zeroes_only
 
       problem = Problem.new(number, path)
       solution = problem.solve
