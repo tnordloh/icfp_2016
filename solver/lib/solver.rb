9c515c1bee5b291fe139043a0654d795f071124f
James Edward Gray II <james@graysoftinc.com>
2016-08-07 10:39:57 -0500

    Bad progress.

--- solver/lib/solver.rb
+++ solver/lib/solver.rb
@@ -27,6 +27,7 @@ class Solver
       scores[number] || 0
     }.each do |path|
       number = File.basename(path, ".txt").to_i
+      next unless number == 4617
 
       old_score = scores[number] || 0
       next if old_score == 1.0
@@ -35,20 +36,20 @@ class Solver
       problem = Problem.new(number, path)
       solution = problem.solve
 
-      if solution && under_size_limit?(solution)
-        begin
-          puts
-          puts "Previous score:  #{old_score}"
+      # if solution && under_size_limit?(solution)
+      #   begin
+      #     puts
+      #     puts "Previous score:  #{old_score}"
 
-          response = api.submit_solution(solution)
-          db.record_if_better(number, response)
+      #     response = api.submit_solution(solution)
+      #     db.record_if_better(number, response)
 
-          p response
-        rescue RestClient::BadRequest => error
-          puts "Error:  #{JSON.parse(error.response.body)["error"]}"
-          puts "Skipping tricky problem: ##{number}"
-        end
-      end
+      #     p response
+      #   rescue RestClient::BadRequest => error
+      #     puts "Error:  #{JSON.parse(error.response.body)["error"]}"
+      #     puts "Skipping tricky problem: ##{number}"
+      #   end
+      # end
     end
   end
 
